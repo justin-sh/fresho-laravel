14d8e1100312f4c5d50bb67648c95e8435d82164
Justin LI <lijt2007@hotmail.com>
2024-09-07 23:22:40 +0930

    add run model

---
+++ database/migrations/2024_09_07_134704_create_runs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('runs', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->string('code',32)->unique();
+            $table->string('name',128);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('runs');
+    }
+};
