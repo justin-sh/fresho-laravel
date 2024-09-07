89a216c9eb97eaf7d7efde2b544239db7de78fe9
Justin LI <lijt2007@hotmail.com>
2024-09-08 00:08:42 +0930

    make order & run unguard

--- app/Models/Run.php
+++ app/Models/Run.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property $id run uuid
+ * @property $code run code
+ * @property $name run name
+ */
 class Run extends Model
 {
     use HasFactory, HasUuids, HasTimestamps;
