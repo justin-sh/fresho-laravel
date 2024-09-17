27100c64b842d7060cf69f27505a1311eb827232
Justin LI <lijt2007@hotmail.com>
2024-09-18 09:27:37 +0930

    update to insert into product_warehouse

--- app/Console/Commands/ProductInit.php
+++ app/Console/Commands/ProductInit.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use App\Models\Product;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Ramsey\Uuid\Uuid;
 
 class ProductInit extends Command
@@ -31,20 +33,39 @@ class ProductInit extends Command
         $products = DB::connection('mysql2')->select('select * from hoc_products');
 
         $prds = [];
-        collect($products)->each(function ($prd) use (&$prds) {
+        $pws = [];
+        collect($products)->each(function ($prd) use (&$prds, &$pws) {
+            $pid = Uuid::uuid4();
             $prds[] = [
-                'id' => Uuid::uuid4(),
+                'id' => $pid,
                 'code' => $prd->prd_code,
                 'name' => $prd->prd_name,
                 'cat' => $prd->cat,
                 'comment' => $prd->comment,
             ];
+            $pws[] = [
+                'pId'=> $pid,
+                'whId'=> '5e9523a6-3f9b-4ae0-b45f-c3a0b97b8936',
+                'qty'=> $prd->onhand_qty,
+                'crat'=> Carbon::now(),
+                'upat'=> Carbon::now(),
+            ];
         });
 
         if($this->option('clean')){
             Product::truncate();
+            DB::delete('delete from product_warehouse');
         }
 
-        Product::upsert($prds, ['id'], []);
+        // Log::debug($prdIds);
+
+        DB::transaction(function() use($prds, $pws){
+            $sql = 'insert into product_warehouse(product_id,warehouse_id,onhand_qty,free_qty,created_at,updated_at) values (:pId,:whId,:qty,0,:crat,:upat)';
+            collect($pws)->each(function($pw) use ($sql) {
+                DB::insert($sql, $pw);
+            });
+
+            Product::upsert($prds, ['id'], []);
+        });
     }
 }
