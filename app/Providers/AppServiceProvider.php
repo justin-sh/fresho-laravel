c25120c9484af89a1c269c46464db11e90d1c846
Justin LI <lijt2007@hotmail.com>
2024-12-23 21:14:34 +1030

    add long sql query log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Listeners\ProductStockEventSubscriber;
+use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\Facades\DB;
@@ -31,7 +32,10 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::preventSilentlyDiscardingAttributes($this->app->isLocal());
 
+        Http::globalOptions(['timeout'=>60]);
+
         Http::globalRequestMiddleware(function (RequestInterface $request) {
+//            Log::debug(gettype($request));
             return $request->withHeader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36')
                 ->withHeader('Accept', 'application/json, text/javascript, */*; q=0.01')
                 ->withHeader('cookie', config('app.fresho.cookie'));
@@ -41,6 +45,15 @@ class AppServiceProvider extends ServiceProvider
 //            Log::debug(Str::padRight('sql', 20) . $query->sql);
 //            Log::debug(Str::padRight('bindings', 10) . json_encode($query->bindings));
 //            Log::debug(Str::padRight('elapsed time', 20) . $query->time);
+        });
+
+        DB::whenQueryingForLongerThan(500, function (Connection $conn, QueryExecuted $q){
+            Log::warning("------- Long sql than 1000ms --------");
+            Log::warning(Str::padRight('conn name', 15) . ':' . $conn->getName());
+            Log::warning(Str::padRight('sql text', 15) . ':' . $q->sql);
+            Log::warning(Str::padRight('sql bindings', 15) . ':' . json_encode($q->bindings));
+            Log::warning(Str::padRight('sql time', 15) . ':' . $q->time . 'milliseconds');
+            Log::warning("------- Long sql than 1000ms --------");
         });
     }
 }
