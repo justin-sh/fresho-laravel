67d3969badeefb63de6294cb2b3a6891310eb3b5
Justin LI <lijt2007@hotmail.com>
2025-01-03 23:18:29 +1030

    update table columns

---
+++ app/Console/Commands/SyncFreshoProducts.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\FreshoProduct;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class SyncFreshoProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fresho:sync-products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync products info from Fresho';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $url = 'https://app.fresho.com/api/v1/companies/b181ee08-2214-46ec-ad1e-926a2bbfb8fb/supplier_product_items';
+        $params = [
+            'order' => 'product+ASC',
+            'q' => '',
+        ];
+
+        $search_after_key = 0;
+        $total_page = 0;
+        $loop = 0;
+
+        while ($search_after_key <= $total_page && $loop < 100) {
+            Log::info("fresho sync product in loop:" . $loop);
+            $loop += 1;
+
+            if ($search_after_key > 0) {
+                $params['search_after_key'] = $search_after_key;
+            }
+            $rv = Http::get($url, $params)->json();
+
+            $total_page = $rv['total_pages'];
+            $search_after_key = $rv['search_after_key'];
+
+            $data = [];
+            foreach ($rv['data'] as $prd) {
+                $nt = explode(' ○ ', $prd['name'], 2);
+
+                $data[] = [
+                    'id' => $prd['id'],
+                    'code' => $prd['code'],
+                    'name' => $nt[0],
+                    'cost' => floatval($prd['cost_price']['price']) * 100,
+                    'price_text' => $prd['edit_prices_link_text'],
+                    'product_id' => $prd['product_id'],
+                    'qty_type' => $nt[1],
+                ];
+
+                if (count($data) >= 10) {
+                    FreshoProduct::upsert($data, ['id'], ['code', 'name', 'cost', 'price_text', 'product_id', 'qty_type']);
+
+                    $data = [];
+                }
+            }
+            FreshoProduct::upsert($data, ['id'], ['code', 'name', 'cost', 'price_text', 'product_id', 'qty_type']);
+        }
+
+        Log::info("fresho sync product finished");
+    }
+}
---
+++ app/Models/FreshoProduct.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
+
+/**
+ * @property Uuid $id
+ * @property String $code
+ * @property String $name
+ * @property integer $cost
+ * @property String $price_text
+ * @property Uuid $product_id
+ * @property String $qty_type
+ */
+class FreshoProduct extends Model
+{
+    use HasFactory, HasTimestamps, HasUuids;
+
+    protected static function booted()
+    {
+        parent::booted();
+        static::unguard();
+    }
+}
