130c084c3ba87b7265b2d299faa9d965dabf11a9
Justin LI <lijt2007@hotmail.com>
2025-01-03 22:39:07 +1030

    update table columns

--- database/migrations/2025_01_03_110308_create_fresho_products.php
+++ database/migrations/2025_01_03_110308_create_fresho_products.php
@@ -13,12 +13,14 @@ return new class extends Migration {
         Schema::create('fresho_products', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('code', 32)->index();
-            $table->string('name', 512)->index();
+            $table->string('name', 512);
             $table->integer('cost')->default(0);
             $table->string('price_text', 512)->nullable();
-            $table->uuid('product_id');
+            $table->uuid('product_id')->index();
             $table->string('qty_type', 32);
             $table->timestamps();
+
+            $table->unique(['name','qty_type'], 'fp_idx_name_qty_type');
         });
     }
 
