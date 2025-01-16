934c78e727dd042bfba19fd981b450d4225f4c9e
Justin LI <lijt2007@hotmail.com>
2025-01-16 21:07:27 +1030

    update the s011 contract

--- resources/js/view/contract/DriverTrainingS011CRM.vue
+++ resources/js/view/contract/DriverTrainingS011CRM.vue
@@ -3,20 +3,53 @@
     <BRow>
         <BCol class="text-center fw-bolder fs-1">S011CRM Training</BCol>
     </BRow>
+    <BRow>
+        <BCol>
+            Make sure follow the below instructions while you drive the UTE S011CRM:
+        </BCol>
+    </BRow>
     <ol>
         <li>
-            112132132
-            <ol type="a">
-                <li>fdad</li>
-                <li>fdaddfdasf</li>
-            </ol>
+            Fuel: <span class="fw-bolder">Petrol, #91</span>
+<!--            <ol type="a">-->
+<!--                <li>Petrol, #91</li>-->
+<!--                <li>fdaddfdasf</li>-->
+<!--            </ol>-->
         </li>
-        <li>12</li>
+        <li>
+            Turn the <span class="fw-bolder">refrigerator</span> OFF and engine OFF every time once arriving the destination
+        </li>
     </ol>
+
 
     <BRow>
         <BCol>
-            <vue-esign class="sign-view" ref="esign" :width="800" :height="300"/>
+            Actions will be applied if failed to follow these instructions. Included but not limited:
+        </BCol>
+    </BRow>
+    <ol>
+        <li>Incident Report</li>
+        <li>Cost to fix the consequences after the violation</li>
+    </ol>
+
+
+
+    <BRow class="mt-4">
+        <BCol>
+            Please sign after reading and understanding all the words:
+        </BCol>
+    </BRow>
+    <BRow>
+        <BCol cols="auto"  class="align-middle my-auto">
+            Accepted by:
+        </BCol>
+        <BCol cols="3">
+            <BInput></BInput>
+        </BCol>
+    </BRow>
+    <BRow class="mt-2">
+        <BCol>
+            <vue-esign class="sign-view" ref="esign" :width="400" :height="150"/>
         </BCol>
     </BRow>
     <BRow>
