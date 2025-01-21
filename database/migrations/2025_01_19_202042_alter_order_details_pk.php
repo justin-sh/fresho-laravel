6bb9e3b8259707f3825186eb8add44a4755e2ef5
Justin LI <lijt2007@hotmail.com>
2025-01-21 20:50:04 +1030

    update db script

--- database/migrations/2025_01_19_202042_alter_order_details_pk.php
+++ database/migrations/2025_01_19_202042_alter_order_details_pk.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,7 @@ return new class extends Migration
     {
         Schema::table('order_details', function (Blueprint $table) {
             $table->integer('idx')->default(1)->after('prd_code');
-            // $table->dropPrimary(['order_number', 'prd_code']);
+            $table->dropPrimary(['order_number', 'prd_code']);
             $table->primary(['order_number', 'prd_code', 'idx']);
         });
     }
@@ -23,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        
+
         Schema::table('order_details', function (Blueprint $table) {
             $table->dropPrimary(['order_number', 'prd_code', 'idx']);
             $table->dropColumn('idx');
